b72ce7777278a2721761e8c7827bfbfb2088724f
ccc <169713467@qq.com>
2022-08-08 23:20:49 +0800

    third commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,27 @@ const path = require('path');
 module.exports = {
     mode:'production',
     entry:path.resolve(__dirname,'src/index.js'),
+    module:{
+        rules:[
+            {
+                test:/\.js/,
+                exclude:/node_modules/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets:['@babel/preset-react']
+                    }
+                }
+            }
+        ],
+    },
     output:{
         path: undefined,
         filename:'bundle.js'
+    },
+    devServer:{
+        port:3001,
+        host:'127.0.0.1',
+        open:true
     }
 }
