35a8f4139a2e8e0a3a0ec2a9a3760271f89015fd
Cstylefly <52144668+Cstylefly@users.noreply.github.com>
2022-08-09 11:24:00 +0800

    webpack.config:update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const setDevServer = () => {
+    return process.env.NODE_ENV === 'development' ? { port:3001,host:'localhost',open:true}:{}
+}
+
+
 module.exports = {
-    mode:'production',
+    mode:process.env.NODE_ENV,
     entry:path.resolve(__dirname,'src/index.js'),
+    output:{
+        path:process.env.NODE_ENV === 'development' ? undefined:path.resolve(__dirname,'dist'),
+        filename:'js/bundle.js',
+        clean: true ,//是否开启每次打包自动清除上一次打包的资源
+    },
     module:{
         rules:[
             {
@@ -16,13 +28,16 @@ module.exports = {
             }
         ],
     },
-    output:{
-        path: undefined,
-        filename:'bundle.js'
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:path.resolve(__dirname,'public/index.html')
+        })
+    ],
+    resolve:{
+        //此配置会自动补全文件后缀
+        extensions:['.js','.jsx','.ts','.tsx','.json']
     },
     devServer:{
-        port:3001,
-        host:'127.0.0.1',
-        open:true
+        ...setDevServer()
     }
 }
